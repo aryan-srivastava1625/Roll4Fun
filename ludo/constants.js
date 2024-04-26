5bdc6617c34916916504322087fda4eb8285e832
ARYAN SRIVASTAVA <103809985+ap9140@users.noreply.github.com>
2024-04-27 04:02:27 +0530

    constant.js

--- ludo/constants.js
+++ ludo/constants.js
@@ -102,3 +102,15 @@ export const HOME_ENTRANCE={
     P1:[100,101,102,103,104],
     P2:[200,201,202,203,204]
 }
+
+export const HOME_POSITIONS={
+    P1:105,
+    P2:205
+}
+
+export const TURNING_POINTS={
+    P1:50,
+    P2:24
+}
+
+export const SAFE_POSITIONS=[0,8,13,21,26,34,39,47];
