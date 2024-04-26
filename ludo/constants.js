d06d8a358e17e4182ba98b120a1c62baa4250b0a
ARYAN SRIVASTAVA <103809985+ap9140@users.noreply.github.com>
2024-04-27 03:58:27 +0530

    constant.js

--- ludo/constants.js
+++ ludo/constants.js
@@ -87,3 +87,18 @@ export const COORDINATES_MAP = {
 
 export const STEP_LENGTH=6.66;
 export const PLAYERS = ['P1', 'P2'];
+
+export const BASE_POSITIONS={
+    P1:[500,501,502,503],
+    P2:[600,601,602,603],
+}
+
+export const START_POSITIONS={
+    P1:0,
+    P2:26
+}
+
+export const HOME_ENTRANCE={
+    P1:[100,101,102,103,104],
+    P2:[200,201,202,203,204]
+}
