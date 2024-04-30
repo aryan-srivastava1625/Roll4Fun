e4376f631411a6d6d209c115b6faf04fda74453c
ARYAN SRIVASTAVA <103809985+ap9140@users.noreply.github.com>
2024-04-30 23:20:33 +0530

    Done with UI.js

--- ludo/Ludo.js
+++ ludo/Ludo.js
@@ -1,6 +1,24 @@
 import { UI } from './UI.js'
 export class Ludo{
+    currentPositions={
+        P1:[],
+        P2:[]
+    }
+
+    _diceValue;
+    get diceValue(){
+        return this._diceValue;
+    }
+    set diceValue(value){
+        this._diceValue=value;
+
+        UI.setDiceValue(value);
+    }
+
     constructor(){
         console.log("Dive into the world of Ludo!");
+        this.diceValue=4;
+        
     }
 }
+
